f2db42b1875fa22dc68deb1dcdb4ff9f415ce18d
Sean Bode <seanpaulbode@gmail.com>
2016-05-20 14:36:22 -0400

    address_book model to include remove_entry. Test created for remove_entry. enty_spec, adddress_book_spec, address_book all modified for indentation.

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,28 +1,32 @@
- require_relative 'entry'
+	require_relative 'entry'
  
 class AddressBook
   attr_reader :entries
   
-     def initialize
-     @entries = []
-   end 
-   
-def add_entry(name, phone_number, email)
-     index = 0
-     entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index += 1
-     end
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
+	def initialize
+		@entries = []
+	end #def
 
-  def remove_entry(name, phone_number, email)
-     index = 0
-     entries.each do |entry|
-       if name == entry.name
-     entries.delete(entry)
-   end
-  end
- end
+   	def add_entry(name, phone_number, email)
+    	index = 0
+     	entries.each do |entry|
+       		if name < entry.name
+         		break
+       		end #if
+       		index += 1
+		end #each .. do
+		entries.insert(index, Entry.new(name, phone_number, email))
+	end #def
+
+	def remove_entry(name, phone_number, email)
+		delete_entry = nil
+		
+		@entries.each do |entry|
+			if name == entry.name && phone_number == entry.phone_number && email == entry.email
+				delete_entry = entry
+			end #if
+		end #do
+		@entries.delete(delete_entry)
+	end # def
+
+end #class
